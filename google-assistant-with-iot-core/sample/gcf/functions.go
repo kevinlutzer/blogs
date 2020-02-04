f90cf3ca2a053e9d351aa0b8526e750fe2d520b5
Kevin Lutzer <klutzer@vendasta.com>
2020-02-03 20:41:28 -0500

    get some more of the work down

--- google-assistant-with-iot-core/sample/gcf/functions.go
+++ google-assistant-with-iot-core/sample/gcf/functions.go
@@ -1,16 +1,40 @@
 package functions
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net/http"
+
+	iotClient "cloud.google.com/go/iot/apiv1"
+	"google.golang.org/api/option"
+	"google.golang.org/genproto/googleapis/cloud/iot/v1"
 )
 
 // HelloWorld
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
-	b, err := ioutil.ReadAll(r.Body)
+	// b, err := ioutil.ReadAll(r.Body)
+	// if err != nil {
+	// 	fmt.Printf("Error: %s", err.Error())
+	// }
+
+	ctx := context.Background()
+	client, err := iotClient.NewDeviceManagerClient(ctx, option.WithCredentialsFile("iot-klutzer-21813b0f64b4.json"))
 	if err != nil {
-		fmt.Printf("Error: %s", err.Error())
+		fmt.Printf("Error Setting Up Client: %s", err.Error())
+		return
 	}
-	fmt.Printf("Body: %s", b)
+
+	bs, _ := json.Marshal(struct {
+		Key string `json:"key"`
+	}{Key: "value"})
+
+	if _, err := client.ModifyCloudToDeviceConfig(ctx, &iot.ModifyCloudToDeviceConfigRequest{
+		Name:       "projects/repcore-prod/locations/us-central1/registries/devices-klutzer/devices/room-environment-monitor-personal",
+		BinaryData: bs,
+	}); err != nil {
+		fmt.Printf("Error Modifying Config: %s", err.Error())
+		return
+	}
+
 }
--- google-assistant-with-iot-core/sample/gcf/go.mod
+++ google-assistant-with-iot-core/sample/gcf/go.mod
@@ -1,3 +1,9 @@
 module functions
 
 go 1.12
+
+require (
+	cloud.google.com/go v0.52.0
+	google.golang.org/api v0.15.0
+	google.golang.org/genproto v0.0.0-20200203223152-ff9e8190c2f5
+)
