1accaa4a892e95d20bc8710ec9a587d1875e96c7
Kevin Lutzer <klutzer@vendasta.com>
2020-02-24 22:53:49 -0600

    have it done

--- iot-core-example/code/main.go
+++ iot-core-example/code/main.go
@@ -1,48 +1,39 @@
 package main
 
 import (
-	"time"
-
+	"fmt"
+	MQTT "github.com/eclipse/paho.mqtt.golang"
 	"gobot.io/x/gobot"
 	"gobot.io/x/gobot/drivers/gpio"
 	"gobot.io/x/gobot/platforms/raspi"
 )
 
 func main() {
-
 	r := raspi.NewAdaptor()
-	led := gpio.NewLedDriver(r, "15")
+	led := gpio.NewLedDriver(r, "10")
 
-	work := func() {
-		gobot.Every(1*time.Second, func() {
-			led.Toggle()
-		})
-	}
-
-	robot := gobot.NewRobot("blinkBot",
+	robot := gobot.NewRobot("unused",
 		[]gobot.Connection{r},
 		[]gobot.Device{led},
-		work,
 	)
 
+	c, err := newClient()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Setup Google IOT Core Config subscription")
+	err = c.Subsribe(configTopic, func(_ MQTT.Client, m MQTT.Message) {
+		if string(m.Payload()) == "ON" {
+			led.On()
+		} else {
+			led.Off()
+		}
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
 	robot.Start()
-
-	// c, err := newClient()
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// fmt.Println("Setup Google IOT Core Config subscription")
-	// if err := c.Subsribe(configTopic, func(_ MQTT.Client, m MQTT.Message) {
-	// 	if len(m.Payload()) == 0 {
-	// 		return
-	// 	}
-	// 	fmt.Printf("Recieved configuration message: %s \n", m.Payload())
-	// }); err != nil {
-	// 	panic(err)
-	// }
-
-	// // Block indefinitely
-	// b := make(chan struct{})
-	// <-b
 }
