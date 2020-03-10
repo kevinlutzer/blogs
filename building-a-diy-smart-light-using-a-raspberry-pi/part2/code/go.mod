817dea85b9cd72ae1af33b253d41044106718e0e
Kevin Lutzer <klutzer@vendasta.com>
2020-03-10 14:14:17 -0600

    remove vendor files

--- building-a-diy-smart-light-using-a-raspberry-pi/part2/code/go.mod
+++ building-a-diy-smart-light-using-a-raspberry-pi/part2/code/go.mod
@@ -2,4 +2,8 @@ module iot-server
 
 go 1.12
 
-require google.golang.org/api v0.20.0
+require (
+	cloud.google.com/go v0.54.0
+	github.com/google/btree v1.0.0 // indirect
+	google.golang.org/genproto v0.0.0-20200305110556-506484158171
+)
--- building-a-diy-smart-light-using-a-raspberry-pi/part2/code/handle_light_state.go
+++ building-a-diy-smart-light-using-a-raspberry-pi/part2/code/handle_light_state.go
@@ -1,12 +1,38 @@
-package stringadderhandler
+package handlelightstate
 
 import (
+	api "cloud.google.com/go/iot/apiv1"
 	"context"
+	"fmt"
+	iotpb "google.golang.org/genproto/googleapis/cloud/iot/v1"
 	"net/http"
-	"google.golang.org/api/cloudiot/v1"
 )
-// StringAdderHandler is a handler that api-ifies the Add method in github.com/kevinlutzer/string-adder
-func StringAdderHandler(w http.ResponseWriter, r *http.Request) {
+
+// HandleLightState state
+func HandleLightState(w http.ResponseWriter, r *http.Request) {
 	ctx := context.Background()
-	cloudiot.NewService(ctx)
+	c, err := api.NewDeviceManagerClient(ctx)
+	if err != nil {
+		msg := fmt.Sprintf("failed to setup device manager: %s", err.Error())
+		fmt.Println(msg)
+		w.Write([]byte(msg))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	_, err = c.ModifyCloudToDeviceConfig(ctx, &iotpb.ModifyCloudToDeviceConfigRequest{
+		Name:       "projects/iot-core-sample-klutzer/locations/us-central1/registries/devices/devices/test-device",
+		BinaryData: []byte("ON"),
+	})
+
+	if err != nil {
+		msg := fmt.Sprintf("failed to update device configuration: %s", err.Error())
+		fmt.Println(msg)
+		w.Write([]byte(msg))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Write([]byte("success"))
+	return
 }
