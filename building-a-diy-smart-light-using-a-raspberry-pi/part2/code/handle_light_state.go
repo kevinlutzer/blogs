7b55198447d972335cd75cbbf9b8c130c3a11a03
Kevin Lutzer <klutzer@vendasta.com>
2020-03-11 20:01:31 -0600

    update the code

--- building-a-diy-smart-light-using-a-raspberry-pi/part2/code/handle_light_state.go
+++ building-a-diy-smart-light-using-a-raspberry-pi/part2/code/handle_light_state.go
@@ -6,10 +6,16 @@ import (
 	"fmt"
 	iotpb "google.golang.org/genproto/googleapis/cloud/iot/v1"
 	"net/http"
+	"strings"
+)
+
+const (
+	projectID = "<PROJECT_ID>"
 )
 
 // HandleLightState state
 func HandleLightState(w http.ResponseWriter, r *http.Request) {
+
 	ctx := context.Background()
 	c, err := api.NewDeviceManagerClient(ctx)
 	if err != nil {
@@ -20,9 +26,23 @@ func HandleLightState(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	q := r.URL.Query()
+	state := strings.ToUpper(
+		strings.Replace(
+			strings.Replace(q.Get("state"), " ", "", -1),
+			"\n", "", -1),
+	)
+	if !(state == "OFF" || state == "ON") {
+		msg := fmt.Sprintf("the state query param must be specified and it must be \"OFF\" or \"ON\". specified value was \"%s\"", state)
+		fmt.Println(msg)
+		w.Write([]byte(msg))
+		w.WriteHeader(http.StatusPreconditionFailed)
+	}
+
+	deviceName := fmt.Sprintf("projects/%s/locations/us-central1/registries/devices/devices/test-device", projectID)
 	_, err = c.ModifyCloudToDeviceConfig(ctx, &iotpb.ModifyCloudToDeviceConfigRequest{
-		Name:       "projects/iot-core-sample-klutzer/locations/us-central1/registries/devices/devices/test-device",
-		BinaryData: []byte("ON"),
+		Name:       deviceName,
+		BinaryData: []byte(state),
 	})
 
 	if err != nil {
